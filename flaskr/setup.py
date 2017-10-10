6f2b553c5ecdb6e504bf77685ee13904f62884a2
Ashik Pirmohamed <ashik1000@hotmail.com>
2017-10-10 20:13:26 +0100

    my first commit

---
+++ flaskr/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup (
+	 name = 'flaskr'
+	,packages=['flaskr']
+	,include_package_data=True
+	,install_require=[
+		'flask',
+	 ]
+	
+
+
+	)
--- routes.py
+++ routes.py
@@ -2,10 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config('SQLALCHEMY_DATABASE_URI') = 'postgresql://localhost/learningflask'
+
+
+
 @app.route("/")
 def index():
 	return render_template("index.html")
 
+
+@app.route("/my_t")
+def mt_t():
+	a = 4
+	b = 2
+	c = a * b
+	return (str(c))
+
+
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
